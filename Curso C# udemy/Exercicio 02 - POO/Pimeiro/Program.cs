e6811a853718c87dafb5fff63dbbbc298a41c4fe
Hugo Barbosa da Silva <70847074+Hugo-BSilva@users.noreply.github.com>
2021-05-13 21:02:44 -0300

    AtualizaçãoNomeArquivos

---
+++ Curso C# udemy/Exercicio 02 - POO/Pimeiro/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sbyte x = 100;
+            //Console.WriteLine(x);
+
+            //double xA, xB, xC, yA, yB, yC;
+
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do Triângulo x: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do Triângulo y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+
+            double areaY = y.Area();
+
+            Console.WriteLine("Área X = " + areaX);
+            Console.WriteLine("Área Y= " + areaY);
+
+            if(areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y"); ;
+            }
+
+        }
+    }
+}
--- Curso C# udemy/Struct/Struct/Program.cs
+++ Curso C# udemy/Struct/Struct/Program.cs
@@ -20,7 +20,7 @@ namespace Struct
             Point p;
             p.x = 10;
             p.y = 20;
-            Console.WriteLine(p);
+            //Console.WriteLine(p);
             p = new Point();
             Console.WriteLine(p);
         }
