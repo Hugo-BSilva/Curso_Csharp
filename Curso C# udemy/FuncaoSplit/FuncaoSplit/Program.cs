76919750291d2cf390520e6f644501a0c68b6a4b
Hugo Barbosa da Silva <70847074+Hugo-BSilva@users.noreply.github.com>
2021-06-07 23:32:46 -0300

    AtualizandoArquivos

---
+++ Curso C# udemy/FuncaoSplit/FuncaoSplit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace FuncaoSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = Console.ReadLine();
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"{nome} {sexo} {idade} {altura}");
+        }
+    }
+}
