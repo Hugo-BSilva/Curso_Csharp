a85f1b27f6683a301cdc7281425d73a28d185f4a
Hugo Barbosa da Silva <70847074+Hugo-BSilva@users.noreply.github.com>
2021-07-08 21:47:15 -0300

    Add_DIO.Series

---
+++ Curso C# udemy/DIO.Series/Classes/Serie.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Genero + Environment.NewLine;
+            retorno += "Descrição: " + this.Genero + Environment.NewLine;
+            retorno += "Ano de início: " + this.Ano;
+            return retorno;
+        }
+    }
+}
