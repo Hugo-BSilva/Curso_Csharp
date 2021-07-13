7a2327d4a5361365a6f2be9759d613a2bedb5707
Hugo Barbosa da Silva <70847074+Hugo-BSilva@users.noreply.github.com>
2021-07-12 21:16:13 -0300

    DEV_Atualizando_Listas1

--- Curso C# udemy/Listas1/Listas1/Program.cs
+++ Curso C# udemy/Listas1/Listas1/Program.cs
@@ -15,7 +15,7 @@ namespace Listas1
             list.Add("joao");
             list.Add("ana");
             list.Add("Joana");
-            list.Add("Alex")
+            list.Add("Alex");
             list.Insert(2, "Marco"); //Adiciona o marco na posição 2
 
             //MOSTRA A LISTA COMPLETA
@@ -27,24 +27,25 @@ namespace Listas1
             Console.WriteLine("List Count: "+ list.Count);//Conta os elementos da lista
 
             //ENCONTRA O PRIMEIRO RESULTADO COM A LETRA M
-            //A expressão LAMBDA x=> x[0] == 'M' representa a função booleana Test
-            string s1 = list.Find(x => x[0] == 'M');
+            //A expressão LAMBDA x=> x[0] == 'M' representa a função booleana Test no final
+            var s1 = list.Find(x => x[0] == 'M');
             Console.WriteLine("First M: "+ s1);
 
             //ENCONTRA O ÚLTIMO RESULTADO COM A LETRA M
-            string s2 = list.FindLast(x => x[0] == 'M');
+            var s2 = list.FindLast(x => x[0] == 'M');
             Console.WriteLine("Last M: "+ s2);
 
             //ENCONTRA O PRIMEIRO COM A LETRA M E RETORNA O NÚMERO DA POSIÇÃO
-            int index = list.FindIndex(x => x[0] == 'M');
+            var index = list.FindIndex(x => x[0] == 'M');
             Console.WriteLine("First position M: "+ index);
 
             //ENCONTRA O ÚLTIMO COM A LETRA M E RETORNA O NÚMERO DA POSIÇÃO
-            int index2 = list.FindLastIndex(x => x[0] == 'M');
+            var index2 = list.FindLastIndex(x => x[0] == 'M');
             Console.WriteLine("Last position M: "+ index2);
 
             //CRIEI UMA SEGUNDA LISTA QUE VAI RETORNAR TODOS OS VALORES QUE TEM 5 CARACTERES
             List<string> list2 = list.FindAll(x => x.Length == 5);
+
             //Dessa forma ele vai percorrer a lista inteira e vai trazer o resultado
             foreach (string obj in list2)
             {
