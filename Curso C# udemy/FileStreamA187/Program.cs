9c7c5cf2a58c74eed64585819cc403d6d8edbe41
Hugo Barbosa da Silva <70847074+Hugo-BSilva@users.noreply.github.com>
2021-07-14 20:38:55 -0300

    Add_UsingBlock_A188

--- Curso C# udemy/FileStreamA187/Program.cs
+++ Curso C# udemy/FileStreamA187/Program.cs
@@ -7,7 +7,7 @@ namespace FileStreamA187
     {
         static void Main(string[] args)
         {
-            string path = @"c:\temp\file1.txt";
+            string path = @"C:\temp\file1.txt";
             //Stream genérica que não faz nenhum tratamento especial pra acessar um recurso de entrada e saída
             FileStream fs = null; 
             //Stream particular otimizada para o que eu preciso
@@ -15,12 +15,13 @@ namespace FileStreamA187
 
             try
             {
-                /* Essas streames são recursos externos, elas conversam com o sistema operacional,
+                /* Esses streams são recursos externos, eles conversam com o sistema operacional,
                  esse recurso não é gerenciado pelo clr do .NET. Como eles não pertencem ao clr
                 vou precisar fechar isso manualmente */
                 //sr = File.OpenText(path)
                 fs = new FileStream(path, FileMode.Open);
                 sr = new StreamReader(fs);
+
                 //Enquanto não chegar no final da stream
                 while (!sr.EndOfStream)
                 {
@@ -35,7 +36,8 @@ namespace FileStreamA187
                 throw;
             }
             finally
-            { //Se o StreamReader estiver aberto
+            { 
+                //Se o FileStream/StreamReader estiverem abertos, vai ser fechado
                 if (sr != null) sr.Close();
                 if (fs != null) sr.Close();
             }
