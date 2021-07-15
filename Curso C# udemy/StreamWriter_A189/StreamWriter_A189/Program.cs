82659b9402c10a1331fc8603370686154fc32d02
Hugo Barbosa da Silva <70847074+Hugo-BSilva@users.noreply.github.com>
2021-07-14 21:12:30 -0300

    Add_UsingBlock_COMPLETE

---
+++ Curso C# udemy/StreamWriter_A189/StreamWriter_A189/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StreamWriter_A189
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Curso C# udemy/UsingBlock_A188/UsingBlock_A188/Program.cs
+++ Curso C# udemy/UsingBlock_A188/UsingBlock_A188/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace UsingBlock_A188
 {
@@ -6,7 +7,42 @@ namespace UsingBlock_A188
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"c:\temp\file1.txt";
+            #region 1ª Forma
+            //try
+            //{
+            //    //Vai ser automaticamente fechado, graças ao using
+            //    using (FileStream fs = new FileStream(path, FileMode.Open))
+            //    {
+            //        using (StreamReader sr = new StreamReader(fs))
+            //        {
+            //            while (!sr.EndOfStream)
+            //            {
+            //                string line = sr.ReadLine();
+            //                Console.WriteLine(line);
+            //            }
+            //        }
+
+            //    }
+            //}
+            #endregion
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }            
         }
     }
 }
