97010dcbf02c0b82f2bfd5a817bbaa5db40ee7dd
Hugo Barbosa da Silva <70847074+Hugo-BSilva@users.noreply.github.com>
2021-07-22 16:56:43 -0300

    Add_dbContextStartup_and_modelContexto

--- Curso C# udemy/CursoMVC/CursoMVC/Models/Contexto.cs
+++ Curso C# udemy/CursoMVC/CursoMVC/Models/Contexto.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CursoMVC.Models
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
+        }
     }
 }
