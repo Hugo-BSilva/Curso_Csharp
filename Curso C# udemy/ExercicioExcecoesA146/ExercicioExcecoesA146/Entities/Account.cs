deecd99bba9120bb948bc0bc3baeebc1c13fe59f
hugo-bsilva <09silvahugo@gmail.com>
2021-11-01 14:59:08 -0300

    ADD - Exercício de Exceções
    Tratando exceções fazendo um programinha de uma conta bancária.

---
+++ Curso C# udemy/ExercicioExcecoesA146/ExercicioExcecoesA146/Entities/Account.cs
@@ -0,0 +1,60 @@
+﻿using ExercicioExcecoesA146.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioExcecoesA146.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+        public Account(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new DomainException("You tried entered amount null.");
+            }
+            else if (amount.GetType() != typeof(int) || amount.GetType() != typeof(double))
+            {
+                throw new DomainException("You no entered numbers.");
+            }
+            else
+            {
+                Balance += amount;
+            }            
+        }
+
+        public void WithDraw(double amount)
+        {
+            if (amount > WithDrawLimit)
+            {
+                throw new DomainException("The amount entered for withdrawal is greater than the limit, please enter a smaller amount.");
+            }
+            else if(amount > Balance)
+            {
+                throw new DomainException("You have no this balance on your account. Your balance is: " + Balance);
+            }
+            else
+            {
+                Balance -= amount;
+            }            
+        }
+    }
+}
---
+++ Curso C# udemy/ExercicioExcecoesA146/ExercicioExcecoesA146/Program.cs
@@ -0,0 +1,66 @@
+﻿using ExercicioExcecoesA146.Entities;
+using ExercicioExcecoesA146.Entities.Exceptions;
+using System;
+
+namespace ExercicioExcecoesA146
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool loop = false;
+
+                do
+                {
+                    Console.WriteLine(" > Enter account data");
+
+                    Console.Write("Number: ");
+                    int numberAccount = int.Parse(Console.ReadLine());
+                    Console.Write("Holder: ");
+                    string holder = Console.ReadLine();
+                    Console.Write("Initial Balance: ");
+                    double initialBalance = double.Parse(Console.ReadLine());
+                    Console.Write("Withdraw Limit: ");
+                    double withDrawLimit = double.Parse(Console.ReadLine());
+
+                    Account account = new Account(numberAccount, holder, initialBalance, withDrawLimit);
+                    //Entre com um valor para sacar 
+                    Console.WriteLine();
+                    Console.Write("Enter amount for whitdraw: ");
+                    double amountWithDraw = double.Parse(Console.ReadLine());
+                    account.WithDraw(amountWithDraw);
+                    //Imprime o valor da conta após o saque
+                    Console.WriteLine("New Balance: " + account.Balance);
+
+                    Console.Write("Do you want to do another operation? Y/N: ");
+                    string op = Console.ReadLine();
+                    Console.Clear();
+
+                    if (string.IsNullOrEmpty(op) || string.IsNullOrWhiteSpace(op)
+                        && op.Trim().ToUpper() == "Y" || op.Trim().ToUpper() == "YES")
+                    {
+                        loop = true;
+                    }
+                    else
+                    {
+                        loop = false;
+                    }
+                } while (loop == false);
+            }
+            catch (DomainException ex)
+            {
+                Console.WriteLine("Is not possible to perform the operation: " + ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Format error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unexpected error: " + ex.Message);
+            }            
+        }
+    }
+}
