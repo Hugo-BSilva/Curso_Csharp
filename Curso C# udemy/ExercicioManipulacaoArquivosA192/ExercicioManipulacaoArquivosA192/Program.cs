dd0df0141e2672a32d8b24cbfb3c78f21dd6e8e2
hugo-bsilva <09silvahugo@gmail.com>
2021-11-01 18:55:03 -0300

    ADD - Exercicio Manipulacao de Arquivos
    Fazendo exercício referente a manipulação de arquivos.

---
+++ Curso C# udemy/ExercicioManipulacaoArquivosA192/ExercicioManipulacaoArquivosA192/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace ExercicioManipulacaoArquivosA192
+{
+    /* Fazer um programa para ler o caminho de um arquivo .csv contendo os dados de itens vendidos. Cada item possui
+     um nome, preço unitário e quantidade, separados por vírgula. Você deve gerar um novo arquivo chamado "summary.csv",
+    localizado em uma subpasta chamada "out" a partir da pasta original do arquivo de origem, contendo apenas o nome
+    e o valor total para aquele item(preço unitário multiplicado pela quantidade).*/
+    class Program
+    {
+        static string diretorioPadrao = @"c:\temp\myfolder";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(" ===> INICIANDO PROCESSAMENTO...");
+                if (Directory.Exists(diretorioPadrao))
+                {
+                    var files = Directory.GetFiles(diretorioPadrao);
+
+                    foreach (var file in files)
+                    {
+                        if (file.Contains(".csv"))
+                        {
+                            Console.WriteLine($"=> Leitura do arquivo: {file} iniciada...");
+                            using (StreamReader sr = new StreamReader(File.OpenRead(file), Encoding.UTF8))
+                            {
+                                int numlinha = 1;
+                                string linha;
+                                string[] produtoLinha;
+
+                                string nomeProduto;
+                                DateTime dataDeValidade;
+                                string valorSTR;
+                                double valorProduto;
+                                int quantidade;
+
+                                while ((linha = sr.ReadLine()) != null)
+                                {
+                                    //Dependendo do formato do arquivo, as informações se dividem por , ou ;
+                                    produtoLinha = linha.Split(",");
+
+
+                                    nomeProduto = produtoLinha[0];
+                                    valorSTR = produtoLinha[1].Replace("R$ ", "").Trim();
+                                    valorProduto = double.Parse(valorSTR.Replace(".", ","));
+                                    quantidade = int.Parse(produtoLinha[2]);
+                                    double valortotal = valorProduto * quantidade;
+
+                                    Console.WriteLine("Leitura da linha: " + numlinha + $"- Nome: {nomeProduto}" +
+                                            $" - Valor do produto: R$: {valorProduto.ToString("F2", CultureInfo.InvariantCulture)}" +
+                                            $" - Quantidade: {quantidade}" +
+                                            $" - Valor total: R$ {valortotal.ToString("F2", CultureInfo.InvariantCulture)}");
+                                    numlinha++;
+
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error ocured: " + e.Message);
+            }
+        }
+    }
+}
+
