9c78e40ce0efb50f2353a06434f5b0af0d5e933b
hugo-bsilva <09silvahugo@gmail.com>
2021-11-01 17:32:33 -0300

    ADD - Path A191
    Aprendendo alguns métodos e propriedades da classe Path.

---
+++ Curso C# udemy/PathA191/PathA191/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace PathA191
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder\file1.txt";
+
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+        }
+    }
+}
