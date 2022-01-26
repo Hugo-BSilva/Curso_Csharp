dd4efa88c05d75e368e7b809c5e73100ee1d71d2
hugo-bsilva <09silvahugo@gmail.com>
2022-01-25 22:34:18 -0300

    Herança Múltipla + Problema do Diamante
    A herança múltipla pode gerar um problema de ambiguidade causada pela existência de um método em mais de uma superclasse, além disso ela não é permitida na maioria das linguagens. Dito isso a classe pai(device) foi alterada para uma classe abstrata para que as filhas implementem ela. Também foi criado as interfaces IPrinter e IScanner que contém os métodos que as duas classes já possuiam. Dessa forma a ComboDevice pode implementar sem nenhum problema os métodos das interfaces e de sua classe pai.

---
+++ Curso C# udemy/InterfacesDiamante/InterfacesDiamante/Devices/ComboDevice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDiamante.Devices
+{
+    class ComboDevice : Device, IScanner, IPrinter
+    {
+        public override void ProcessDoc(string document)
+        {
+            Console.WriteLine("ComboDevice print " + document);
+        }
+
+        public string Scan()
+        {
+            return "ComboDevice scan result";
+        }
+
+        public void Print(string document)
+        {
+            Console.WriteLine("ComboDevice processing " + document);
+        }
+    }
+}
