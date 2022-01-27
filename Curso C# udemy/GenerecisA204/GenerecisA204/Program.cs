19fcbcfdbfc0086b2ea0b3e484ac4365f8415008
hugo-bsilva <09silvahugo@gmail.com>
2022-01-27 20:15:07 -0300

    Genericis - Início aula 204

---
+++ Curso C# udemy/GenerecisA204/GenerecisA204/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenerecisA204
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
