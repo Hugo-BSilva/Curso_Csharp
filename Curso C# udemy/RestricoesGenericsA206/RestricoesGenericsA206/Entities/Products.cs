5339a29cd14e69b85aea078922bbfe18df31f481
hugo-bsilva <09silvahugo@gmail.com>
2022-01-31 21:43:42 -0300

    Restrições Generics - Final aula 206
    Restrição de Generic:
    public T Max<T>(List<T> list) where T : IComparable
    
    OBS: há outros tipos de restrição que é possível fazer além dessa

---
+++ Curso C# udemy/RestricoesGenericsA206/RestricoesGenericsA206/Entities/Products.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestricoesGenericsA206.Entities
+{
+    class Products : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Products(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name +
+                ", " +
+                Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Products))
+            {
+                throw new ArgumentException("Comparing error: argument is not an Product");
+            }
+
+            Products other = obj as Products;
+
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
--- Curso C# udemy/RestricoesGenericsA206/RestricoesGenericsA206/Program.cs
+++ Curso C# udemy/RestricoesGenericsA206/RestricoesGenericsA206/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using RestricoesGenericsA206.Entities;
+using RestricoesGenericsA206.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace RestricoesGenericsA206
 {
@@ -6,7 +10,24 @@ namespace RestricoesGenericsA206
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Products> listProducts = new List<Products>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            CalculationService calculationService = new CalculationService();
+
+            for (int i = 0; i < n; i++)
+            {
+                //Esse vetor vai separar o nome do produto do preço
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                listProducts.Add(new Products(name, price));
+            }
+
+            Console.WriteLine("MAX: ");
+            Console.WriteLine(calculationService.Max(listProducts));
         }
     }
 }
