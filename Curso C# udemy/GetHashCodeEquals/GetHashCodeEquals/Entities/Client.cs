b4c2ab62f37452fb60a4c41042fc96a134afd1c9
hugo-bsilva <09silvahugo@gmail.com>
2022-02-01 21:32:16 -0300

    GetHashCode e Equals - Final aula 207
    Modo de utilização exemplo: em uma lista com milhares de registros, faça a busca pelo GetHashCode, se encontrar o elemento que estava procurando então faça uma última verificação usando o Equals.

---
+++ Curso C# udemy/GetHashCodeEquals/GetHashCodeEquals/Entities/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHashCodeEquals.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Client))
+            {
+                //throw new ArgumentException("This object is not an Client");
+                return false;
+            }
+
+            Client other = obj as Client;
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
