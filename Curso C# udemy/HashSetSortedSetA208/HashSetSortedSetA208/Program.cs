488f61d8fe2d0d488c98c9f766e7aeb77b7bcc53
hugo-bsilva <09silvahugo@gmail.com>
2022-02-01 22:11:09 -0300

    HashSet e SortedSet - Início aula 208
    São coleções extremamente rápidas nas operações de inserção, remoção e busca.

---
+++ Curso C# udemy/HashSetSortedSetA208/HashSetSortedSetA208/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetSortedSetA208
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine();
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
