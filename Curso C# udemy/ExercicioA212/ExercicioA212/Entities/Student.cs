9914fe903cd16192b2cc1c053355de7aa1b1b5ea
hugo-bsilva <09silvahugo@gmail.com>
2022-03-01 16:04:48 -0300

    Exercício Proposto RESOLVIDO - aula 212

---
+++ Curso C# udemy/ExercicioA212/ExercicioA212/Entities/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioA212.Entities
+{
+    class Student
+    {
+        public int Registration { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Registration.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Student))
+            {
+                return false;
+            }
+            Student other = obj as Student;
+
+            return Registration.Equals(other.Registration);
+        }
+    }
+}
---
+++ Curso C# udemy/ExercicioA212/ExercicioA212/Program.cs
@@ -0,0 +1,40 @@
+﻿using ExercicioA212.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace ExercicioA212
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Student> set = new HashSet<Student>();
+
+            Console.Write("How many students for course A? ");
+            int manyStudentA = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= manyStudentA; i++)
+            {
+                int registration = int.Parse(Console.ReadLine());
+                set.Add(new Student { Registration = registration});
+            }            
+
+            Console.Write("How many students for course B? ");
+            int manyStudentB = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= manyStudentB; i++)
+            {
+                int registration = int.Parse(Console.ReadLine());
+                set.Add(new Student { Registration = registration });
+            }            
+
+            Console.Write("How many students for course C? ");
+            int manyStudentC = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= manyStudentC; i++)
+            {
+                int registration = int.Parse(Console.ReadLine());
+                set.Add(new Student { Registration = registration });
+            }
+
+            Console.WriteLine("Total Students: " + set.Count);
+        }
+    }
+}
