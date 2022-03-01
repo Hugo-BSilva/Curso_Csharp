5c1fdcc438817cc473bee5f1df3f3fcf2ee46be8
hugo-bsilva <09silvahugo@gmail.com>
2022-03-01 00:39:49 -0300

    Igualdade utilizando Hash

---
+++ Curso C# udemy/HashSetSortedSetA210/HashSetSortedSetA210/Entities/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSetSortedSetA210.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        /// <summary>
+        /// O método deve ser implementado para que a comparação do HashSet aconteça da forma correta
+        /// (fazendo a verificação pelo código do objeto, não pelo endereço do mesmo)
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Price.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            //Se o obj não for um produto
+            if (!(obj is Product))
+            {
+                return false;
+            }
+            Product other = obj as Product;
+
+            return Name.Equals(other.Name) && Price.Equals(other.Price);
+        }
+    }
+}
---
+++ Curso C# udemy/HashSetSortedSetA210/HashSetSortedSetA210/Program.cs
@@ -0,0 +1,25 @@
+﻿using HashSetSortedSetA210.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace HashSetSortedSetA210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 500.00));
+            a.Add(new Product("Celular", 800.00));
+            a.Add(new Product("Notebook", 1200.00));
+
+            HashSet<Point> b= new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1200.00);
+            //Sem a implementação do método = FALSE
+            Console.WriteLine(a.Contains(prod)); 
+        }
+    }
+}
