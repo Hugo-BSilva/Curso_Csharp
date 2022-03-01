441544bd0acc9502247aed930b69c6cb9b43e5db
hugo-bsilva <09silvahugo@gmail.com>
2022-03-01 16:44:47 -0300

    Dictionary e SortedDictionary - aula 213
    Finalizada, testes com algumas funções dictionary

---
+++ Curso C# udemy/DictionarySortedDictionaryA213/DictionarySortedDictionaryA213/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionarySortedDictionaryA213
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99999999";
+            cookies["user"] = "mariana";
+            cookies["adress"] = "brazil";
+
+            Console.WriteLine(cookies["user"]);
+            cookies.Remove("adress");
+            Console.WriteLine("Many cookies? " + cookies.Count);
+
+            Console.WriteLine("ALL COOKIES:");           
+
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
